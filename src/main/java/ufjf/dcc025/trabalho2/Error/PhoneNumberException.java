4a63619b3ecc67256e960d47d3c5e975477c7ae6
ItaloFSilver <fagundes.italo@estudante.ufjf.br>
2025-12-26 22:18:53 -0300

    Data incluida, metodo de cadastrar paciente na Secretary

---
+++ src/main/java/ufjf/dcc025/trabalho2/Error/PhoneNumberException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ufjf.dcc025.trabalho2.Error;
+
+/**
+ *
+ * @author Ircasa
+ */
+public class PhoneNumberException extends Exception {
+    public PhoneNumberException(String mensagem){
+        super(mensagem);
+    }
+}
