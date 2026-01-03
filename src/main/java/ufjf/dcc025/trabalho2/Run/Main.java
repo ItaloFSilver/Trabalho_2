9509ab47ed04b237ec8140eabadaab54dbba0965
ItaloFSilver <fagundes.italo@estudante.ufjf.br>
2026-01-03 19:52:01 -0300

    criadas interfaces de login e secretaria. Funcionalidade of

--- src/main/java/ufjf/dcc025/trabalho2/Run/Main.java
+++ src/main/java/ufjf/dcc025/trabalho2/Run/Main.java
@@ -1,7 +1,14 @@
+
 package ufjf.dcc025.trabalho2.Run;
 
+import ufjf.dcc025.trabalho2.UserInterface.MainFrame;
+
 public class Main {
+    static MainFrame janela = new MainFrame();
+    
     public static void main(String[] args) {
+        janela.setVisible(true);
         System.out.println("Hello world!");
+        
     }
 }
