1624f1b2fbe643d624eeb998374b369f6e4c36b9
Italo <fagundes.italo@estudante.ufjf.br>
2026-01-07 15:20:40 -0300

    testes de CPF e Telefone Criados

---
+++ src/test/java/br/ufjf/dcc025/trabalho2/model/CPFTest.java
@@ -0,0 +1,42 @@
+package br.ufjf.dcc025.trabalho2.model;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.ufjf.dcc025.trabalho2.model.Credentials.CPF;
+import br.ufjf.dcc025.trabalho2.model.Error.InvalidCPFException;
+
+public class CPFTest {
+    
+    @Test
+    @DisplayName("Deve criar um CPF válido sem lançar exceções")
+    void shouldNotThrowWhenCreatingValidCPF() {
+        String validCPF = "11111111111";
+        assertDoesNotThrow(() -> new CPF(validCPF));
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção ao criar um CPF inválido")
+    void shouldThrowWhenCreatingInvalidCPF() {
+        String invalidCPF = "12345678901";
+        InvalidCPFException e = assertThrows(InvalidCPFException.class, () -> {new CPF(invalidCPF);});
+        assertEquals("CPF invalido", e.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve considerar dois CPFs iguais como iguais")
+    void shouldTestEquals() {
+        String cpf1 = "02355209693";
+        String cpf2 = "02355209693";
+        String cpf3 = "11111111111";
+        CPF test1 = new CPF(cpf1);
+        CPF test2 = new CPF(cpf2);
+        CPF test3 = new CPF(cpf3);
+        assertEquals(test1, test2);
+        assertNotEquals(test1, test3);
+    }
+}
---
+++ src/test/java/br/ufjf/dcc025/trabalho2/model/PhoneTest.java
@@ -0,0 +1,30 @@
+package br.ufjf.dcc025.trabalho2.model;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.ufjf.dcc025.trabalho2.model.Credentials.Telefone;
+import br.ufjf.dcc025.trabalho2.model.Error.InvalidTelefoneException;
+
+public class PhoneTest {
+    
+    @Test
+    @DisplayName("Deve criar um Telefone válido sem lançar exceções")
+    void shouldNotThrowWhenCreatingValidTelefone() {
+        String validPhone = "32999239026";
+        assertDoesNotThrow(() -> new Telefone(validPhone));
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção ao criar um CPF inválido")
+    void shouldThrowWhenCreatingInvalidTelefone() {
+        String invalidPhone = "12ATELEFONE0";
+        InvalidTelefoneException e = assertThrows(InvalidTelefoneException.class, () -> {new Telefone(invalidPhone);});
+        assertEquals("Telefone invalido", e.getMessage());
+    }
+
+}
