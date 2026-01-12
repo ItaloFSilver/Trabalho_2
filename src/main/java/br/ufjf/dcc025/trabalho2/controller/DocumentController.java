ade711e4e20385a043fd3b229390af2a38bde9f5
Artsm12 <16869841624@estudante.ufjf.br>
2026-01-11 23:49:52 -0300

    aaaaaaaaaaa

--- src/main/java/br/ufjf/dcc025/trabalho2/controller/DocumentController.java
+++ src/main/java/br/ufjf/dcc025/trabalho2/controller/DocumentController.java
@@ -1,4 +1,8 @@
-package br.ufjf.dcc025.trabalho2.controller;
+
+/*
+*Arthur de Souza Marques - 202435015
+*Ítalo Fagundes Silvério - 202435020
+*/package br.ufjf.dcc025.trabalho2.controller;
 
 import java.io.File;
 import java.io.FileReader;
